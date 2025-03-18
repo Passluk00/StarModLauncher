0218b15d8a406460f2a0546705654bdb434cc9f6
prova 1 <>
2025-03-18 13:43:40 +0100

    Refactoring mainPage and implementation of scrollable Bar

--- core/config.py
+++ core/config.py
@@ -1,5 +1,5 @@
 name = "Mod Launcher Star Citizen"
 version = "v1.0"
 config_file = "config.json"
-minDim = (1200, 1200)    # Dimensione minima della finestra 
-                        # Viene usata anche per la grandezza di default per avvio della finestra
+minDim = (1000, 800)    # Dimensione minima della finestra 
+#          x    y      # Viene usata anche per la grandezza di default per avvio della finestra
--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -9,173 +9,104 @@ from customtkinter import CTkImage
 from CTkColorPicker import *
 
 class MainContent(ctk.CTkFrame):
-    
-    def __init__(self,parent, setup):
-        
+    def __init__(self, parent, setup):
         super().__init__(parent)
-        
-        self.currentPath= None
+        self.currentPath = None
         self.setup = setup
-        
-        
+        # DA Rifattorizzare
         self.logo = Image.open("ui/asset/MadeByTheCommunity_Black.png")
-        
         self.logo_img = CTkImage(light_image=self.logo, dark_image=self.logo, size=(150, 150))
-        
         self.labelImg = ctk.CTkLabel(self, image=self.logo_img, text="")
         self.labelImg.pack(pady=10)
-        
         self.label = ctk.CTkLabel(self, text="To get Started Select a Project or Create One").pack(padx=4, pady=20)
-      
-        ## Aggiungere una Label con il nome del Progetto
-        
-        
+
+    ## Aggiungere una Label con il nome del Progetto
+    # Nuovo codice
     def updateContent(self, project_name):
-        
-        
         # Variabili da Modificare
-        
-        self.color1 = "#65aaf2"                  # Scritta Rsi
-        self.color2 = "#a6cef7"                  # Scritta Rsi Hover
-        self.color3 = "#152b3d"                  # Scritte Rsi BackGround Color
-        self.color4 = "#0d1a25"                  # Scritte Rsi BackGround Color Hover
-        self.color5 = "#FFFFFF"                  # Scritte Bianche
-        
-        
+        self.color1 = "#65aaf2"  # Scritta Rsi
+        self.color2 = "#a6cef7"  # Scritta Rsi Hover
+        self.color3 = "#152b3d"  # Scritte Rsi BackGround Color
+        self.color4 = "#0d1a25"  # Scritte Rsi BackGround Color Hover
+        self.color5 = "#FFFFFF"  # Scritte Bianche
         # Update Setup
-        
         self.currentPath = self.setup.ReadPath(project_name)
         self.projectName = project_name
         for widget in self.winfo_children():
             widget.destroy()
-    
         self.loadColor(project_name)
+        # Creazione del canvas scrollabile
+        container = ctk.CTkScrollableFrame(self)
+        container.pack(fill="both", expand=True)
+
+        # Widget
+        ctk.CTkLabel(container, text=f"Project: {project_name}", font=("Roboto", 30, "bold")).pack(pady=10)
+
+        # widget for modifying colors
+        self.add_color_row(container, [
+            ("Scritta Rsi", self.select_color1, self.color1),
+            ("Scritta Rsi Hover", self.select_color2, self.color2)
+        ])
+        self.add_color_row(container, [
+            ("Scritta Rsi Background Color", self.select_color3, self.color3),
+            ("Scritta Rsi Background Color Hover", self.select_color4, self.color4)
+        ])
+        self.add_color_row(container, [
+            ("Scritta Biache", self.select_color5, self.color5),
+        ])
+
+        # Bottone per salvare i colori nel file
+        ctk.CTkButton(container, text="Salva i colori", command=self.salvaSuFile).pack(pady=10)
+        ctk.CTkButton(container, text="Applica colori").pack(pady=10)
+        ctk.CTkButton(container, text="Stampa tutti i colori", command=self.printVal).pack(pady=10)
+
+    def add_color_row(self, parent, color_data):
+        row_frame = ctk.CTkFrame(parent, fg_color="transparent")
+        row_frame.pack(pady=4, padx=10, fill='x')
+        num_elements = len(color_data)
+        for i, (text, command, initial_color) in enumerate(color_data):
+            frame = ctk.CTkFrame(row_frame, fg_color="transparent")
+            frame.pack(side='left', padx=10, expand=True) # expand True per centrare
+            label_frame = ctk.CTkFrame(frame, fg_color="transparent")
+            label_frame.pack(pady=2)
+            ctk.CTkLabel(label_frame, text=text, font=("Tekton Pro", 20, "bold")).pack()
+            CTkColorPicker(frame, width=300, command=command, initial_color=initial_color).pack(pady=2)
+
+
     
     
-        # Widget 
-    
-        ctk.CTkLabel(self, text=f"Project: {project_name}",font=("Roboto", 30, "bold")).grid(row=0, column=1, pady=10, padx=10)
-        ctk.CTkLabel(self, text="").grid(row=0, column=7)
-        
-        
-        # Space
-        ctk.CTkLabel(self,text="").grid(row=1)
-        
-        
-        ###   Test code
-        
-            # test con pyqt5
-                # self.web_preview = WebPreviewQ(self.currentPath)
-                # self.web_preview.show_preview()
-                
-            # Crea una finestra di WebPreview con tkinterweb
-                # self.window = WebPreview(self,self.currentPath)
-                # self.window.pack(fill="both", expand=True, padx=5, pady=5)
-                
-        ###
-        
-        # widget for modify the Scritta Rsi color
-        self.color1Lable = ctk.CTkLabel(self, text="Scritta Rsi",font=("Tekton Pro", 20, "bold")).grid(row=2, column=0, pady=4, padx=10)
-        self.colorpicker = CTkColorPicker(self, width=300, command=self.select_color1, initial_color=(self.color1)).grid(row=3, column=0, pady=4, padx=10)
-        
-        
-        # widget for modify the Scritta Rsi Hover Color
-        self.color2Lable = ctk.CTkLabel(self, text="Scritta Rsi Hover",font=("Tekton Pro", 20, "bold")).grid(row=2, column=2, pady=4, padx=10)
-        self.colorpicker2 = CTkColorPicker(self, width=300, command=self.select_color2, initial_color=(self.color2)).grid(row=3, column=2, pady=4, padx=10)
-        
-        
-        # Space
-        ctk.CTkLabel(self,text="").grid(row=4)
-        # Space
-        
-        
-        # widget for modify the BackGround Color Scritta RSI
-        self.color3Lable = ctk.CTkLabel(self, text="Scritta Rsi Background Color",font=("Tekton Pro", 20, "bold")).grid(row=5, column=0, pady=4, padx=10)
-        self.colorpicker3 = CTkColorPicker(self, width=300, command=self.select_color3, initial_color=(self.color3)).grid(row=6, column=0, pady=4, padx=10)
-        
-        
-        # widget for modify the BackGround Color Scritta RSI Hover
-        self.color4Lable = ctk.CTkLabel(self, text="Scritta Rsi Background Color Hover",font=("Tekton Pro", 20, "bold")).grid(row=5, column=2, pady=4, padx=10)
-        self.colorpicker4 = CTkColorPicker(self, width=300, command=self.select_color4, initial_color=(self.color4)).grid(row=6, column=2, pady=4, padx=10)
-        
-        
-        # Space
-        ctk.CTkLabel(self,text="").grid(row=7)
-        # Space
-        
-        
-        # widget for modify the White Scritta Color
-        self.colo51Lable = ctk.CTkLabel(self, text="Scritta Biache",font=("Tekton Pro", 20, "bold")).grid(row=8, column=0, pady=4, padx=10)
-        self.colorpicker5 = CTkColorPicker(self, width=300, command=self.select_color5, initial_color=(self.color5)).grid(row=9, column=0, pady=4, padx=10)
-        
-        
-        # widget for modify the  Color
-       # self.color1Lable = ctk.CTkLabel(self, text="  ",font=("Tekton Pro", 20, "bold")).grid(row=8, column=2, pady=4, padx=10)
-       # self.colorpicker2 = CTkColorPicker(self, width=300, command=self.select_color2).grid(row=9, column=2, pady=4, padx=10)
-        
-        
-        # Bottone per salvare i colori inel file
-        ctk.CTkButton(self, text="Salva i colori", command=self.salvaSuFile).grid(row=11, column=1, pady=10)
-        
-        
-        # Bottone per applicare i colori direttamente sul file 
-        ctk.CTkButton(self, text="Applica colori").grid(row=12, column=1, pady=10)
-        
-        
-        # Bottone per verificare i colori salvati
-        ctk.CTkButton(self, text="Stampa tutti i colori", command=self.printVal).grid(row=13, column=1, pady=10)
-        
-        
-    # Utility Function
-        
-        
-    def select_color1(self,color):
+    def select_color1(self, color):
         self.color1 = color
-        print(f"Colore 1 Salvato: {self.color1}")
-        
-    
-    def select_color2(self,color):
+
+    def select_color2(self, color):
         self.color2 = color
-        print(f"Colore 2 Salvato: {self.color2}")
-        
-    def select_color3(self,color):
-        self.color3= color
-        print(f"Colore 3 Salvato: {self.color3}")
-        
-    def select_color4(self,color):
+
+    def select_color3(self, color):
+        self.color3 = color
+
+    def select_color4(self, color):
         self.color4 = color
-        print(f"Colore 4 Salvato: {self.color4}")
-        
-    def select_color5(self,color):
+
+    def select_color5(self, color):
         self.color5 = color
-        print(f"Colore 5 Salvato: {self.color5}")
-        
-    
+
     def printVal(self):
-        print("\n")
-        print(f"Colore 1: {self.color1}")
-        print(f"Colore 2: {self.color2}")
-        print(f"Colore 3: {self.color3}")
-        print(f"Colore 4: {self.color4}")
-        print(f"Colore 5: {self.color5}")
-        print("\n")
-        
-        
+        print(f"Colore 1: {self.color1}\nColore 2: {self.color2}\nColore 3: {self.color3}\nColore 4: {self.color4}\nColore 5: {self.color5}\n")
+
     def salvaSuFile(self):
-        
         colors = {
-            "col1": self.color1,
-            "col2": self.color2,
-            "col3": self.color3,
-            "col4": self.color4,
+            "col1": self.color1, 
+            "col2": self.color2, 
+            "col3": self.color3, 
+            "col4": self.color4, 
             "col5": self.color5
         }
-       
         self.setup.SaveData(self.projectName, "colors", colors)
-        
+
     def loadColor(self, projectname):
-        
         self.colors = self.setup.LoadData(projectname, "colors")
-        
         self.color1 = self.colors["col1"]
-        
+        self.color2 = self.colors["col2"]
+        self.color3 = self.colors["col3"]
+        self.color4 = self.colors["col4"]
+        self.color5 = self.colors["col5"]
